cbaf749565ba46fedce21ae89f975b20e8911e1b
vasanth <vk432@njit.edu>
2017-07-10 14:54:39 -0400

    Updated the list of classes

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hey all');
+// List of classes
+
+var Player() = function() {};
+
+var Deck() = function() {};
+
+var Shuffle() = function() {};
+
+var Card() = function() {};
+
+var Deal() = function() {};
+
+var Game() = function() {};
