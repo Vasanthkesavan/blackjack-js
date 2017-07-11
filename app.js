5c1f99a9ee169feaf3adbbe611378e15bcaa70de
vasanth <vk432@njit.edu>
2017-07-11 13:59:50 -0400

    Added psuedocode for the classes

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 // List of classes
 
-var Player() = function() {};
+var Player() = function() {
+  // Give the default cash to start the game
 
-var Deck() = function() {};
+  // Get the cards from the dealer
+};
 
-var Shuffle() = function() {};
+var Deck() = function() {
+  // Give the type of cards 2..9, A, J, Q, K
 
-var Card() = function() {};
+  // Give the suit type
+};
 
-var Deal() = function() {};
+var Shuffle() = function() {
+  // Get the set of decks from the Deck class
 
-var Game() = function() {};
+  // Do math.random to mix the card into random order
+};
+
+var Card() = function() {
+  // Get the first card
+
+  // return the value of a single card
+
+    // Can be called twice to return the first two cards by default
+};
+
+var Dealer() = function() {
+  // Give  a new deck
+
+    // Shuffle the above deck
+
+  // Get the card
+
+  // Get the input for bet/ input to either hit / stand
+
+
+};
+
+var Game() = function() {
+  // Instantiate player class i.e one player
+
+  // Instantiate a dealer
+
+  // Set the default cash value for player
+
+  // Keep checking for blackjack
+
+    // if blackjack reset both player and dealer
+};
