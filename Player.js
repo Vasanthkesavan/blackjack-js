a9dd8e731ba4d9830f94ca93ca67ac4f0720bf78
vasanth <vk432@njit.edu>
2017-07-14 15:43:56 -0400

    Added a hand method

--- Player.js
+++ Player.js
@@ -1,37 +1,22 @@
 const Deck = require('./Deck.js');
 
 class Player {
-  constructor() {
-
+  constructor(props) {
+    this.props = props;
   }
-  // Give the default cash to start the game
 
-  // Get the cards from the dealer
+  hand() {
+    return this.props;
+  }
 
   hit() {
-  // Check the score before hit
 
-  // Call the card and get a single card value
-
-  // Check if less than 21
-
-    // if, then ask for the decision again
-
-  // If greater, lose
   }
 
   stand() {
-  // Don't take a card
 
-  // Dealer shows the second card
-
-  // Dealer takes a card(dealer's hit)
-
-  // Calculate the total of dealer and player
-
-  // If player > dealer, player BLACKJACK
-
-  // else dealer wins
   }
 
 };
+
+module.exports = Player;
